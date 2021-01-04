9a4e9a6ab69e8489ee66150472c0f6adee0cf1d0
aindriu80 <aindriu80@gmail.com>
2021-01-04 11:25:52 +0000

    Connecting to API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import Movie from "./components/Movie";
 
-function App() {
-  const movies = ["1", "2,", "3"];
+const FEATURED_API = process.env.REACT_APP_API_KEY_ENV;
 
-  return (
-    <div>
-      {movies.map((movie) => (
-        <Movie />
-      ))}
-    </div>
-  );
+// const IMG_API = process.env.IMG_API_ENV;
+// const SEARCH_API = process.env.IMG_API_ENV;
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=c8d8c40713c2164f10bc006b246301eb&page=1"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Movie = () => <div classname="movie">This is a movie component</div>;
+const Movie = () => <div className="movie">This is a movie component</div>;
 
 export default Movie;
